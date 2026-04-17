cf0def54aa9e0d44159f3d94d5faacdc766f6e4d
Ethan Luh <ethanluh@gmail.com>
2026-04-17 15:38:25 -0400

    Create Lab12 files

---
+++ Lab12/compile.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+gcc Lab1201_EthanLuh.c -o Lab1201_EthanLuh.out
+gcc Lab1202_EthanLuh.c -o Lab1202_EthanLuh.out
+gcc Lab1203_EthanLuh.c -o Lab1203_EthanLuh.out
+gcc Lab1204_EthanLuh.c -o Lab1204_EthanLuh.out
+
