56e291e9053db44856f6b43753a51be564a9a104
Ethan Luh <ethanluh@gmail.com>
2026-04-24 17:03:36 -0400

    Complete Lab13

---
+++ Lab13/Lab1301_EthanLuh.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+
+#define N 5
+
+sem_t forks[N];
+
+int left(int p) { return p; }
+int right(int p) { return (p + 1) % N; }
+
+void *philosopher(void *num);
+void think() { sleep(1); }
+void eat() { sleep(1); }
+void get_forks(int p);
+void put_forks(int p);
+
+int main() {
+    pthread_t philosophers[N];
+    int phil_num[N];
+
+    for (int i = 0; i < N; i++) {
+        sem_init(&forks[i], 0, 1);
+        phil_num[i] = i;
+        pthread_create(&philosophers[i], NULL, philosopher, &phil_num[i]);
+    }
+
+    for (int i = 0; i < N; i++) {
+        pthread_join(philosophers[i], NULL);
+    }
+
+    return 0;
+}
+
+void *philosopher(void *num) {
+    int p = *(int *)num;
+    
+    think();
+    get_forks(p);
+    printf("Philosopher %d start eating.\n", p);
+    eat();
+    put_forks(p);
+    printf("Philosopher %d finish eating.\n", p);
+}
+
+void get_forks(int p) {
+    if (p == N-1) {
+        sem_wait(&forks[right(p)]);
+        printf("Philosopher %d picks right fork.\n", p);
+        sem_wait(&forks[left(p)]);
+        printf("Philosopher %d picks left fork.\n", p);
+    } else {
+        sem_wait(&forks[left(p)]);
+        printf("Philosopher %d picks left fork.\n", p);
+        sem_wait(&forks[right(p)]);
+        printf("Philosopher %d picks right fork.\n", p);
+    }
+}
+
+void put_forks(int p) {
+    sem_post(&forks[left(p)]);
+    printf("Philosopher %d returns left fork.\n", p);
+    sem_post(&forks[right(p)]);
+    printf("Philosopher %d returns right fork.\n", p);
+}
+
---
+++ Lab13/compile.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+gcc -pthread Lab1301_EthanLuh.c -o Lab1301_EthanLuh.out
+gcc -pthread Lab1302_EthanLuh.c -o Lab1302_EthanLuh.out
+
