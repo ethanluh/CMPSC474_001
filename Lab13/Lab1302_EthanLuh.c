9718e79d1ebd0edddc37261124d7fd4aa94c7200
Ethan Luh <ethanluh@gmail.com>
2026-04-24 17:29:43 -0400

    Fix Lab13 tasks 1 and 2 to use atomic execution

--- Lab13/Lab1302_EthanLuh.c
+++ Lab13/Lab1302_EthanLuh.c
@@ -7,6 +7,7 @@
 #define N 5
 
 sem_t forks[N];
+sem_t atomic_execution;  // Semaphore for atomic execution
 
 int left(int p) { return p; }
 int right(int p) { return (p + 1) % N; }
@@ -21,10 +22,11 @@ int main() {
     pthread_t philosophers[N];
     int phil_num[N];
 
+    sem_init(&atomic_execution, 0, 1);  // Initialize atomic execution semaphore
     for (int i = 0; i < N; i++) {
         sem_init(&forks[i], 0, 1);
         phil_num[i] = i;
-        pthread_create(&philosophers[i], NULL, philosopher, &phil_num[i]);
+        pthread_create(&philosophers[i], NULL, philosopher, (phil_num + i));
     }
 
     for (int i = 0; i < N; i++) {
@@ -37,12 +39,16 @@ int main() {
 void *philosopher(void *num) {
     int p = *(int *)num;
     
+    sem_wait(&atomic_execution);  // Acquire atomic execution lock
+    
     think();
     get_forks(p);
     printf("Philosopher %d start eating.\n", p);
     eat();
     put_forks(p);
     printf("Philosopher %d finish eating.\n", p);
+    
+    sem_post(&atomic_execution);  // Release atomic execution lock
 }
 
 void get_forks(int p) {
